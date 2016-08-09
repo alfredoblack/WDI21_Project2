69bea5e64c377868588e3e11617b9cdcbc00373a
Adriana Black <adrianablancov@gmail.com>
2016-08-09 18:52:52 +0100

    homepage formatted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
   resources :events
+  
   post '/events/:id/attend', to: "events#attend", as: :attend_event
   delete '/events/:id/leave', to: "events#leave", as: :leave_event
 
@@ -8,9 +9,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, except: [:new, :create]
+  post '/private', to:'private#index'
   
   get '/public', to: 'public#index'
-  get '/private', to: 'private#index'
+  # get '/private', to: 'private#index'
   get '/signed_in', to: 'private#signed_in'
 
   # get 'public/index'
